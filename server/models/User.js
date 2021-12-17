3818ab05a00ed43e243090ea53ec8f065fc0112c
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2021-12-17 13:09:42 +0200

    making a small server to play with at the front end

---
+++ server/models/User.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String,
+});
+
+userSchema.pre('save', function(next) {
+    bcrypt.genSalt(10)
+        .then((salt) => {
+            return bcrypt.hash(this.password, salt)
+        })
+        .then((hashedPassword) => {
+            this.password = hashedPassword;
+        })
+        .catch((err) => console.log('Failed to hash the user\' password', +err));
+});
+
+
+const user = mongoose.model('User', userSchema);
+
+module.exports = user;
--- server/server.js
+++ server/server.js
@@ -1,17 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const { PORT } = require('./constants');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
 
+app.get('/users', (req, res) => {
+    res.json({ ok: true });
+});
 
+app.post('/login', (req, res) => {
+    console.log(req.body);
+});
 
 
 
 
 mongoose.connect('mongodb+srv://PetyrDjunov:secretPassword@cubicles.hdzkr.mongodb.net/auth?retryWrites=true&w=majority')
     .then(() => {
-        app.listen(PORT, () => console.log(`Server is running on ${PORT}...`))
+        app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}...`))
     })
     .catch(() => console.log('Database failed to connect'));
